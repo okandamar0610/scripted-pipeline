c833ed44d83289fa7044aaddf9da24ce95648fe9
Okan Damar <okandamar@Okans-MacBook-Pro-6.local>
2020-08-22 14:38:02 -0700

    Jenkins-input was created

---
+++ Jenkinsfile-input.groovy
@@ -0,0 +1,15 @@
+node {
+  stage("Building"){
+    echo 'Building..'
+  }
+  stage("Testing"){
+    echo 'Testing..'
+  }
+  stage ('Input'){
+      input 'Do you want to continue?'
+  }
+  stage("Deploying "){
+    echo 'Deploying....'
+  }
+  
+}
