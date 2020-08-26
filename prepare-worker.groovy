0430e895b09704df3bcc86f640af218dd7eb9285
Okan Damar <okandamar.com>
2020-08-26 00:30:26 +0000

    added

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,7 +1,13 @@
 node {
-    stage('Init') {
-        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY root@104.131.115.78 yum install epel-release -y'
+    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+        stage('Init') {
+            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@134.122.123.53 yum install epel-release -y'
+        }
+        stage("Install git") {
+            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@134.122.123.53 yum install git -y'
+        }
+        stage("Install Java"){
+            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@134.122.123.53 yum install java-1.8.0-openjdk-devel -y'
         }
     }
 }
