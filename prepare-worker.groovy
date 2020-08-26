ef79f87ff735264f7cd39a03e41bb93f8d9b8196
Okan Damar <okandamar.com>
2020-08-26 00:57:27 +0000

    parameters added

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,13 +1,18 @@
+properties([
+    parameters([
+        string(defaultValue: '', description: 'Please enter VM IP', name: 'nodeIP', trim: true)
+        ])
+    ])
 node {
     withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
         stage('Init') {
-            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@134.122.123.53 yum install epel-release -y'
+            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install epel-release -y'
         }
         stage("Install git") {
-            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@134.122.123.53 yum install git -y'
+            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install git -y'
         }
         stage("Install Java"){
-            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@134.122.123.53 yum install java-1.8.0-openjdk-devel -y'
+            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install java-1.8.0-openjdk-devel -y'
         }
     }
 }
