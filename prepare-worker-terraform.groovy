94e34a67db6f2ce7d23329227402d1cfd99675e4
Okan Damar <okandamar.com>
2020-09-01 23:08:04 +0000

    tasks are added

---
+++ prepare-worker-terraform.groovy
@@ -0,0 +1,26 @@
+properties([
+    parameters([
+        string(defaultValue: '', description: 'Please enter VM IP', name: 'nodeIP', trim: true)
+        ])
+    ])
+if (nodeIP?.trim()) {
+    node {
+        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+            stage('Init') {
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install epel-release -y'
+            }
+            stage("Install git") {
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install git -y'
+            }
+            stage("Install Java"){
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install java-1.8.0-openjdk-devel -y'
+            }
+            stage("Install Ansible"){
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install ansible -y'
+            }
+        }
+    }
+}
+else {
+    error 'Please enter valid IP address'
+}
